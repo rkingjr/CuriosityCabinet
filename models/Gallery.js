2458f71ca08f069dc2a32bbd10da0ade026997c7
Randy King Jr <rkingjr@outlook.com>
2022-02-11 12:38:37 -0600

    add file structure

---
+++ models/Gallery.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Gallery extends Model {}
+
+Gallery.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    starting_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    ending_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'gallery',
+  }
+);
+
+module.exports = Gallery;
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Gallery = require('./Gallery');
+const Painting = require('./Painting');
+
+Gallery.hasMany(Painting, {
+  foreignKey: 'gallery_id',
+});
+
+Painting.belongsTo(Gallery, {
+  foreignKey: 'gallery_id',
+});
+
+module.exports = { User, Gallery, Painting };
