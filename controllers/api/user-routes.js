ba6bddfdaf253854b8931f2f1015c54e2d08ecb9
Julie Chu <julieychu@gmail.com>
2022-02-17 01:01:29 -0600

    login and css fixes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -8,7 +8,7 @@ const withAuth = require('../../utils/auth');
 router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
-      username: req.body.username,
+      name: req.body.name,
       email: req.body.email,
       password: req.body.password,
     });
