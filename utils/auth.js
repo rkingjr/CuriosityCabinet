02e1b15b90fc1b8534c90991b7ac5005b69cbffd
Julie Chu <julieychu@gmail.com>
2022-02-19 16:32:31 -0600

    final css fixes

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ const withAuth = (req, res, next) => {
   if (!req.session.loggedIn) {
     res.redirect('/login');
   } else {
-    // If the user is logged in, execute the route function that will allow them to view the gallery
+    // If the user is logged in, execute the route function that will allow them to upload image and then add annotations
     // We call next() if the user is authenticated
     next();
   }
